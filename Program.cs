8a62f53b052b50642d1949b7abb86c0a77a17531
Asoji <99072163+asoji@users.noreply.github.com>
2022-03-15 12:57:11 -0700

    :loud_sound: Added Easy Log

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using EasyLog;
 
 namespace HorizonZeroDawnDRPC {
     class Program {
-        static void Main(string[] args) {
+        public static Logger log = new Logger();
+        public static Config cfg = new Config();
+
+        static void SetConfig() {
+            // cfg.LogPath = "";
+            cfg.Date = true;
+            cfg.Console = true;
+            cfg.ClearOnStart = true;
+        }
+        public static void Main(string[] args) {
+            log.cfg = cfg;
+            SetConfig();
+
             DiscordRichPresence.Setup();
             HorizonZeroDawnProcessCheck.ProcessCheck();
             Console.ReadKey(true);
